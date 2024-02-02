4f52c78038ed6923ca2895a36bf1bebeb0c932af
JaiJai <jianliang@ceis.tw>
2024-02-02 13:49:03 +0800

    先存

--- assets/Script/Asset/SpriteAtlasAsset.ts
+++ assets/Script/Asset/SpriteAtlasAsset.ts
@@ -1,10 +1,9 @@
-import { assetManager, error, ImageAsset, JsonAsset, log, resources, SpriteAtlas, SpriteFrame, Texture2D } from "cc";
-import Image from "../../Plug/Image";
-import { NotificationType } from "../Enum/NotificationType";
-
+import { resources, SpriteAtlas, SpriteFrame } from "cc";
 import { GameEvent } from "../Enum/GameEvent";
+import { NotificationType } from "../Enum/NotificationType";
 import IAsset from "../Interface/IAsset";
 import EventMng from "../Manager/EventMng";
+
 
 class SpriteAtlasAsset implements IAsset {
     data: Map<string, SpriteFrame> = new Map();
@@ -40,60 +39,14 @@ class SpriteAtlasAsset implements IAsset {
 
         })
         return this
-        //遠端測試
-        // let URL = "http://10.20.13.202/JaiJaiTest/Remote/planets"
-        // assetManager.loadRemote(URL + ".plist", (err, plist: JsonAsset) => {
-        //     if (err) {
-        //         error(err.message)
-        //         return
-        //     }
-        //     assetManager.loadRemote(URL + ".png", (err, image: ImageAsset) => {
-        //         if (err) {
-        //             error(err.message)
-        //             return
-        //         }
-        //         let atlas = Image.transformTextureToSpriteAtlas(plist, image)
-        //         atlas.getSpriteFrames().forEach(_spriteFrame => {
-        //             console.log(_spriteFrame);
-        //             _Asset.set(_spriteFrame.name, _spriteFrame)
-        //         })
-        //         this.checkAssetState = true;
-        //     })
-        // })
-        for (let index = 0; index < this.urlPath.length; index++) {
-            let URL = this.urlPath[index]
-            assetManager.loadRemote(URL + ".plist", (err, plist: JsonAsset) => {
-                if (err) {
-                    error(err.message)
-                    return
-                }
-                assetManager.loadRemote(URL + ".png", (err, image: ImageAsset) => {
-                    if (err) {
-                        error(err.message)
-                        return
-                    }
-                    let atlas = Image.transformTextureToSpriteAtlas(plist, image)
-                    atlas.getSpriteFrames().forEach(_spriteFrame => {
-                        // console.log(_spriteFrame);
-                        // _Asset.set(_spriteFrame.name, _spriteFrame)
-                    })
-                    // this.checkAssetState = true;
-                })
-            })
-
-        }
-
     }
     loadLogo(lang: string) {
         let formatArr = []
         formatArr.push(this.typePath + "Lang/" + lang)
         resources.load(formatArr, SpriteAtlas, (err, data: Array<SpriteAtlas>) => {
-            // console.log(err);
-            // console.log(data);
 
             data.forEach(_SpriteAtlas => {
                 _SpriteAtlas.getSpriteFrames().forEach(_spriteFrame => {
-                    // console.log(_spriteFrame);
                     this.data.set(_spriteFrame.name, _spriteFrame)
                 })
             });
